40e5ff622415c94712b0a52f2318863329c50f6f
Lucy Merlier <66435395+LucyMerlier@users.noreply.github.com>
2021-03-10 17:07:25 +0100

    added intitial set of files

---
+++ script.js
@@ -0,0 +1,17 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const playerName = prompt("What's you name?");
+let guessNumber = parseInt(prompt("Guess the number (between 1 and 100 included)"));
+let round = 1;
+
+while (guessNumber !== randomNumber) {
+    if (isNaN(guessNumber)) {
+        guessNumber = parseInt(prompt("Give me a number you idiot!"));
+    } else if (guessNumber > randomNumber) {
+        guessNumber = parseInt(prompt("It is less! Try again! :P"));
+    } else if (guessNumber < randomNumber) {
+        guessNumber = parseInt(prompt("It is more! Try again! :P"));
+    }
+    round++;
+}
+
+alert("Congratulations, " + playerName + "! You win in " + round + " rounds! :D");
